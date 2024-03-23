a525b1bc9946d8b03e11b94a8d74f35818a82c11
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-03-23 21:50:53 +0530

    Added Send Email Verification code functionality

--- src/configs/email.config.js
+++ src/configs/email.config.js
@@ -5,7 +5,7 @@ import ApiError from "../utils/apiError.js";
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
